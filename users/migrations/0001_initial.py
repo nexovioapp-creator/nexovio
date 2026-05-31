41752ef61e8c5577324c4819f051b89f1d5687b6
Ashoka <nexovioapp@gmail.com>
2026-05-31 15:00:12 +0530

    Fix migration dependency

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('mobiles', '0002_wishlist_table'),
+        ('mobiles', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
