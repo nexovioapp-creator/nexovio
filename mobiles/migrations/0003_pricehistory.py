7c5b79ea71b8e7dd860c8764236731c9e310ab2e
Ashoka <nexovioapp@gmail.com>
2026-05-31 12:57:38 +0530

    Fix migration dependency

--- mobiles/migrations/0003_pricehistory.py
+++ mobiles/migrations/0003_pricehistory.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mobiles', '0002_wishlist_table'),
+        ('mobiles', '0001_initial'),
     ]
 
     operations = [
